f8b740d49e097f61df957927e751786d068c6a56
SophieAu <SophieAu@users.noreply.github.com>
2015-12-20 11:42:44 +0100

    The ScreenDimensions variables need to be looked at again
    They need to be lower case, since they aren't constants and they should
    be uninitialized to begin with and then initialized in the
    DefaultScreenOptions class

--- src/spiel/ScreenDimensions.java
+++ src/spiel/ScreenDimensions.java
@@ -5,5 +5,6 @@ package spiel;
  *
  */
 public class ScreenDimensions {
+	//TODO: these babies need to be in lower case and they should be initialized in the DefaultScreenOptions class (as far as I can tell)
 	static final int WIDTH = 800, HEIGHT = 600;
 }

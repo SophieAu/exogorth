1ece0131c26cd2eaf8a5daed4e585b1d61155a3e
SophieAu <SophieAu@users.noreply.github.com>
2016-06-12 17:40:38 +1000

    Renamed Window.java to Settings.java
    Now holds all global settings, not just the ones for the window

--- src/exogorth/Settings.java
+++ src/exogorth/Settings.java
@@ -1,10 +1,14 @@
 package exogorth;
 
-public class Window {
+public class Settings {
 	public static final int WIDTH = 800, HEIGHT = 600;
 
 	// Menu-specific constants
 	public static final int BOXWIDTH = 350, BOXHEIGHT = 50;
-	public static final int BOXX = (Window.WIDTH - BOXWIDTH) / 2;
+	public static final int BOXX = (Settings.WIDTH - BOXWIDTH) / 2;
 	public static final int BOXYSPACE = 80;
+	
+	//Level-specific constants
+	public static final int LENGTH = 3000;
+	
 }
